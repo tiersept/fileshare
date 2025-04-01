555173614375c23a4ff98ed41f1578a0d20dd9f5
Kevin Lobine <i@kevinlobine.com>
2025-04-01 21:48:13 +0200

    add iterm

---
+++ iterm/Oscura.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Oscura Dusk
+printf "\033]P0131419\033\\"  # Background
+printf "\033]P1FF5C5C\033\\"  # Red
+printf "\033]P24EBE96\033\\"  # Green
+printf "\033]P3D2D714\033\\"  # Yellow
+printf "\033]P4479FFA\033\\"  # Blue
+printf "\033]P5E6E7A3\033\\"  # Magenta
+printf "\033]P654C0A3\033\\"  # Cyan
+printf "\033]P7E6E6E6\033\\"  # White
+printf "\033]P846474F\033\\"  # Bright Black
+printf "\033]P9FF5C5C\033\\"  # Bright Red
+printf "\033]Pa4EBE96\033\\"  # Bright Green
+printf "\033]PbD2D714\033\\"  # Bright Yellow
+printf "\033]Pc479FFA\033\\"  # Bright Blue
+printf "\033]PdE6E7A3\033\\"  # Bright Magenta
+printf "\033]Pe54C0A3\033\\"  # Bright Cyan
+printf "\033]PfE6E6E6\033\\"  # Bright White
+printf "\033]Pg5A5B63\033\\"  # Selection
+printf "\033]Ph131419\033\\"  # Background
+printf "\033]PlE6E6E6\033\\"  # Cursor
+printf "\033]Pj131419\033\\"  # Selected Text Background
+printf "\033]Pk131419\033\\"  # Selected Text
+printf "\033]PiE6E6E6\033\\"  # Bold
